44c80d212963d3f730220d558c6f31559e75e4bf
iliabespalov <105886843+iliabespalov@users.noreply.github.com>
2026-05-19 17:08:12 +1000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,5 @@ fun property(name: String) = properties.getProperty(name) ?: ""
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("com.android.library") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
